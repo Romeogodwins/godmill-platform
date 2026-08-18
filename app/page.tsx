8435fb4bfeb392137988045e6c1607dc41f0b657
Godwin Dagadu <romeogodwins79@gmail.com>
2026-08-18 21:11:03 +0200

    feat: add guesthouse structured data for Google

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,111 @@ import TestimonialsSection from "./components/home/TestimonialsSection";
 import LocationSection from "./components/home/LocationSection";
 import FooterSection from "./components/home/FooterSection";
 import WhatsAppButton from "./components/layout/WhatsAppButton";
+
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "LodgingBusiness",
+  "@id": "https://www.godmillcityguesthouse.com/#lodgingbusiness",
+
+  name: "Godmill City Guesthouse",
+
+  url: "https://www.godmillcityguesthouse.com",
+
+  telephone: "+27790582637",
+
+  description:
+    "Godmill City Guesthouse provides comfortable and affordable accommodation in Taung, North West, South Africa, including executive, standard and family rooms.",
+
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "217 Khibitswane, Cokonyane Road",
+    addressLocality: "Taung",
+    addressRegion: "North West",
+    postalCode: "8584",
+    addressCountry: "ZA",
+  },
+
+  priceRange: "R500-R850",
+
+  amenityFeature: [
+    {
+      "@type": "LocationFeatureSpecification",
+      name: "Free WiFi",
+      value: true,
+    },
+    {
+      "@type": "LocationFeatureSpecification",
+      name: "Secure Parking",
+      value: true,
+    },
+    {
+      "@type": "LocationFeatureSpecification",
+      name: "Air Conditioning",
+      value: true,
+    },
+    {
+      "@type": "LocationFeatureSpecification",
+      name: "Breakfast Available",
+      value: true,
+    },
+  ],
+
+  makesOffer: [
+    {
+      "@type": "Offer",
+      name: "Standard Room without Air Conditioning",
+      price: "500",
+      priceCurrency: "ZAR",
+    },
+    {
+      "@type": "Offer",
+      name: "Standard Room with Air Conditioning",
+      price: "600",
+      priceCurrency: "ZAR",
+    },
+    {
+      "@type": "Offer",
+      name: "Executive Room",
+      price: "750",
+      priceCurrency: "ZAR",
+    },
+    {
+      "@type": "Offer",
+      name: "Family 3 Sleeper without Air Conditioning",
+      price: "750",
+      priceCurrency: "ZAR",
+    },
+    {
+      "@type": "Offer",
+      name: "Family 3 Sleeper with Air Conditioning",
+      price: "850",
+      priceCurrency: "ZAR",
+    },
+  ],
+};
+
 export default function Home() {
   return (
-    <main className="bg-[#080808] text-white">
-      <Navbar />
-      <HeroSection />
-      <RoomsSection />
-      <AmenitiesSection />
-      <GallerySection />
-      <StatsSection />
-      <TestimonialsSection />
-      <LocationSection />
-      <FooterSection />
-      <WhatsAppButton />
-    </main>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(structuredData),
+        }}
+      />
+
+      <main className="bg-[#080808] text-white">
+        <Navbar />
+        <HeroSection />
+        <RoomsSection />
+        <AmenitiesSection />
+        <GallerySection />
+        <StatsSection />
+        <TestimonialsSection />
+        <LocationSection />
+        <FooterSection />
+        <WhatsAppButton />
+      </main>
+    </>
   );
 }
